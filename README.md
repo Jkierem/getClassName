05c8bf3ac8c5a9fb838bcae95ba8f6be60a0ffbd
Jkierem <juanmiguelgomez8@gmail.com>
2020-08-18 12:04:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# getClassName
+
+Simple function for managing css classes. It was made with react in mind
+
+## Installation
+
+```shell
+yarn add getclassname
+// or
+npm install getclassname
+```
+
+## Usage
+
+It is a function that returns a string with a couple more methods. The common use cases are as follows:
+
+```javascript
+import getClassName from 'getclassname';
+
+const common = getClassName({
+    "class1": true,
+    "not-included": false,
+    "class2": () => true
+})
+
+// common === "class1 class2"
+
+const interpolation = getClassName({
+    base: "base",
+    "&--active": true,
+})
+
+// interpolation === "base base--active"
+
+const extended = interpolation.extend("&__extended");
+
+// extended === "base__extended"
+
+const extra = interpolation.extend("&__extended").recompute({
+    "&--active": false
+})
+
+// extra === "base__extended base__extended--active"
+
+const changeInterpolationToken = getClassName({
+    base: "base",
+    token: "$",
+    "$--different": true
+})
+
+// changeInterpolationToken === "base base--different"
+```
+
+And that's all there is...
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,40 @@
+import { default as gcn } from './'
+
+describe("getClassNameTests", () => {
+    it("should return the expected classes", () => {
+        const expected = "base base--active base--expected";
+        const result = gcn({
+            base: "base",
+            "&--active": true,
+            "&--expected": () => true,
+            "&--unexpected": () => false
+        })
+        expect(result).toEqual(expected);
+    })
+
+    it("should allow extension", () => {
+        const expected = "base--extended";
+        const result = gcn({ base: "base" }).extend("&--extended");
+        expect(result).toEqual(expected);
+    })
+
+    it("should allow recomputing after extension", () => {
+        const expected = "base--extended base--extended--recomputed";
+        const result = gcn({ base: "base" }).extend("&--extended").recompute({
+            "&--recomputed": true
+        });
+        expect(result).toEqual(expected);
+    })
+
+    it("should allow changing the interpolation token", () => {
+        const expected = "base base--active base--expected";
+        const result = gcn({
+            base: "base",
+            token: "%",
+            "%--active": true,
+            "%--expected": () => true,
+            "%--unexpected": () => false
+        })
+        expect(result).toEqual(expected);
+    })
+})
